5bb04a7a59a884a97cb58bf6fe2938b843a1219d
skiriel <gabrieldllopes@gmail.com>
2023-03-22 19:25:50 -0300

    feat: Added option to remove car by searching the plate license

--- Estacionamento/Program.cs
+++ Estacionamento/Program.cs
@@ -14,7 +14,7 @@ namespace Estacionamento
                 new Car("Francisca", "Fiat", "Silver", "BYA8807"),
                 new Car("Roberval", "Uno", "Black", "UYY0069"),
                 new Car("Cloud", "Fenrir", "Black", "SOL6137"),
-                new Car("Mario", "Kart", "White", "ITS84710"),
+                new Car("Mario", "Kart", "White", "ITS8471"),
             };
 
             foreach (Car car in cars)
@@ -22,11 +22,18 @@ namespace Estacionamento
                 garagem.AddCar(car);
             }
 
+            Console.WriteLine("BEM VINDO À GARAGEM DO GABRIEL! SEGUEM ABAIXO OS CARROS CONOSCO HOJE:" + Environment.NewLine);
+            foreach (Car car in garagem.Veiculos)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.WriteLine();
             char resp;
             do
             {
-                Console.WriteLine("Deseja adicionar um carro? s/n");
-                resp = char.Parse(Console.ReadLine());
+                Console.WriteLine("Deseja ADICIONAR um carro? ('s' para confirmar) ");
+                resp = char.Parse(Console.ReadLine().ToLower());
 
                 if (resp == 's')
                 {
@@ -41,14 +48,43 @@ namespace Estacionamento
 
                     Car carro = new Car(nome, modelo, cor, placa);
                     garagem.AddCar(carro);
+                    Console.WriteLine("ADICIONADO!");
+                    Console.WriteLine();
                 }
 
             } while (resp == 's');
 
+            do
+            {
+                Console.WriteLine("Deseja REMOVER um carro? ('s' para confirmar) ");
+                resp = char.Parse(Console.ReadLine().ToLower());
+
+                if (resp == 's')
+                {
+                    Console.WriteLine("Digite o NÚMERO DA PLACA para encontrarmos seu carro");
+                    string placa = Console.ReadLine();
+                    if (garagem.SearchCar(placa) != null)
+                    {
+                        garagem.RemoveCar(garagem.SearchCar(placa));
+                        Console.WriteLine("REMOVIDO!");
+                    } else
+                    {
+                        Console.WriteLine("Opa! Essa placa não consta no nosso banco de dados!");
+                    }
+                    Console.WriteLine();
+                }
+
+            } while (resp == 's');
+
+            Console.WriteLine();
+            Console.WriteLine("NOSSA GARAGEM HOJE FECHA COM OS SEGUINTES CARROS:" + Environment.NewLine);
             foreach (Car car in garagem.Veiculos)
             {
                 Console.WriteLine(car);
             }
+
+            Console.WriteLine();
+            Console.WriteLine("OBRIGADO PELA PRESENÇA E ATÉ AMANHÃ!");
 
         }
     }
